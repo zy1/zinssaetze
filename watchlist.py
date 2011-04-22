8b678e8fb6eec5d9c3f23f842ef0b5d09a0dbbfc
zy <zeyer.dr.g@googlemail.com>
2011-04-22 23:58:40 +0200

    Liste erzeugen

--- watchlist.py
+++ watchlist.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python
 
+for line in open("watchlist.csv").readlines():
+	entries = [ e.replace("\"", "").replace(",", ".").replace for e in line.split(";") ]
+	if len(entries) == 0: continue
+	if entries[0] != "Aktien": continue
+	entries[3]
+
+	print entries[2], entries[3], entries[5], entries[9], entries[10]
 
